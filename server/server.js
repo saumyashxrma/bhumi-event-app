1656cd92ac3d6fd0bd599ddd863997b60a0bf829
saumya335 <saumyashxrma@gmail.com>
2025-06-12 16:46:00 +0530

    step 5 : connected mongodb using mongoose

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
-const express=require('express');
-const cors=require('cors');
-const app=express();
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const connectDB = require('./config/db')
 
-app.use(cors());
-app.use(express.json());
+const app = express()
 
-app.get('/',(req,res)=>{
-    res.send('API is running');
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...')
 })
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-});
+// Connect DB and start server
+connectDB()
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
