a1799a6ecf0345db9f22a6a35c4b8959bf9baa1e
saumya335 <saumyashxrma@gmail.com>
2025-06-12 13:50:52 +0530

    Step 1 complete : setup react with tailwind and vite

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100 text-3xl font-bold">
+      Tailwind is working 🎉
+    </div>
+  );
+}
+
+
+export default App
